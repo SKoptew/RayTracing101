3e46adc581abef9067751cd487cfc0a25de99406
Sergey Koptev <skoptew@gmail.com>
2020-10-01 01:22:19 +0300

    vec3 RandUnitVector() - faster implementation based on Marsaglia's paper

--- RayTracing101/src/Math101.cpp
+++ RayTracing101/src/Math101.cpp
@@ -24,27 +24,27 @@ vec3 SrgbToLinear(const vec3 &c)
 static std::mt19937 gen{ std::random_device() () };
 
 real Rand01()
-{   
-    //static std::uniform_real_distribution<real> distr{ 0, 1 };
-    //return distr(gen);
-
+{
     return std::generate_canonical<real, std::numeric_limits<real>::digits>(gen);
 }
 
 vec3 RandUnitVector()
 {
-    //-- rejection sampling
+    //-- Marsaglia, George. Choosing a Point from the Surface of a Sphere. Ann. Math. Statist. 43 (1972), no. 2, 645--646
     vec3 pt;
-    real length2;
     do
     {
-        pt.x = Rand01() * 2 - 1;
-        pt.y = Rand01() * 2 - 1;
-        pt.z = Rand01() * 2 - 1;
+        pt.x = Rand01() * 2 - 1;      // v1
+        pt.y = Rand01() * 2 - 1;      // v2
+        pt.z = pt.x*pt.x + pt.y*pt.y; // S
+    }
+    while (pt.z >= 1);
 
-        length2 = pt.length2();
-    } 
-    while (length2 > 1);
+    real m = 2 * sqrt(pt.z);
 
-    return pt / sqrt(length2);
+    pt.x *= m;
+    pt.y *= m;
+    pt.z = 1 - 2*pt.z;
+
+    return pt;
 }
